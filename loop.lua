ce28d65455dd7c45af869f862da63777d00986a2
Naton-Cai <natoncai@gmail.com>
2024-06-30 21:38:46 -0700

    d

--- loop.lua
+++ loop.lua
@@ -1,16 +1,17 @@
 local dfpwm    = require("cc.audio.dfpwm")
 local decoder  = dfpwm.make_decoder()
 local speakers = {peripheral.find("speaker")}
-
-for chunk in io.lines("data/example.dfpwm", 16 * 1024) do
-    	local buffer = decoder(chunk)
-	local play_functions = {}
-	for _, speaker in pairs(speakers) do
-		table.insert(play_functions, function()
-    			while not speaker.playAudio(buffer) do
-        			os.pullEvent("speaker_audio_empty")
-    			end
-		end)
+while true do
+	for chunk in io.lines("data/example.dfpwm", 16 * 1024) do
+			local buffer = decoder(chunk)
+		local play_functions = {}
+		for _, speaker in pairs(speakers) do
+			table.insert(play_functions, function()
+					while not speaker.playAudio(buffer) do
+						os.pullEvent("speaker_audio_empty")
+					end
+			end)
+		end
+		parallel.waitForAll(table.unpack(play_functions))
 	end
-	parallel.waitForAll(table.unpack(play_functions))
 end
